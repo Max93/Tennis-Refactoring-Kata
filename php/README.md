ee2abee46e45bfb0eef24067d2c3e70954b5adb8
Massimo Ruggirello <massimoruggirello@gmail.com>
2017-06-12 10:07:41 +0200

    move score logic inide an array of feedbacks

--- php/README.md
+++ php/README.md
@@ -1,3 +1,8 @@
+# Refactoring
+After refactoring the logic are inside an array of Feedbacks. In this way we don't need a lot of controls because we use the player's score like index.
+The code now looks more understandable and clear.
+Moreover this logic uncouple the string value from logic; this means that in few seconds we can change a feedback text or language without changing the core logic.
+
 # Run the tests
 1. Install [composer](https://getcomposer.org) locally:
 
--- php/TennisGame1.php
+++ php/TennisGame1.php
@@ -7,6 +7,23 @@ class TennisGame1 implements TennisGame
     private $player1Name = '';
     private $player2Name = '';
 
+    private $feedbacks = [
+        'tie' => [
+            'Love-All',
+            'Fifteen-All',
+            'Thirty-All',
+            'Deuce'
+        ],
+        'lead' => [
+            'Love',
+            'Fifteen',
+            'Thirty',
+            'Forty'
+        ],
+        'win' => 'Win for',
+        'advantage' => 'Advantage'
+    ];
+
     public function __construct($player1Name, $player2Name)
     {
         $this->player1Name = $player1Name;
@@ -24,58 +41,14 @@ class TennisGame1 implements TennisGame
 
     public function getScore()
     {
-        $score = "";
-        if ($this->m_score1 == $this->m_score2) {
-            switch ($this->m_score1) {
-                case 0:
-                    $score = "Love-All";
-                    break;
-                case 1:
-                    $score = "Fifteen-All";
-                    break;
-                case 2:
-                    $score = "Thirty-All";
-                    break;
-                default:
-                    $score = "Deuce";
-                    break;
-            }
-        } elseif ($this->m_score1 >= 4 || $this->m_score2 >= 4) {
-            $minusResult = $this->m_score1 - $this->m_score2;
-            if ($minusResult == 1) {
-                $score = "Advantage player1";
-            } elseif ($minusResult == -1) {
-                $score = "Advantage player2";
-            } elseif ($minusResult >= 2) {
-                $score = "Win for player1";
-            } else {
-                $score = "Win for player2";
-            }
-        } else {
-            for ($i = 1; $i < 3; $i++) {
-                if ($i == 1) {
-                    $tempScore = $this->m_score1;
-                } else {
-                    $score .= "-";
-                    $tempScore = $this->m_score2;
-                }
-                switch ($tempScore) {
-                    case 0:
-                        $score .= "Love";
-                        break;
-                    case 1:
-                        $score .= "Fifteen";
-                        break;
-                    case 2:
-                        $score .= "Thirty";
-                        break;
-                    case 3:
-                        $score .= "Forty";
-                        break;
-                }
-            }
-        }
-        return $score;
+        if ($this->m_score1 == $this->m_score2)
+            return array_key_exists($this->m_score1 , $this->feedbacks['tie']) ? $this->feedbacks['tie'][$this->m_score1] : end($this->feedbacks['tie']);
+        elseif ($this->m_score1 >= 4 || $this->m_score2 >= 4) {
+            $diff = $this->m_score1 - $this->m_score2;
+            $player = $diff > 0 ? 'player1' : 'player2';
+            return $this->feedbacks[abs($diff) > 1 ? 'win' : 'advantage'] . " " . $player;
+        } else
+            return $this->feedbacks['lead'][$this->m_score1] . "-" . $this->feedbacks['lead'][$this->m_score2];
     }
 }
 
